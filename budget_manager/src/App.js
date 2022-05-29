24b923af0436e1f4c71fcb47890ba07c184dd5e8
Shahil <shahilpatel809@gmail.com>
2022-05-29 14:01:21 +0530

    tried out background picture
    failed attempt.

--- budget_manager/src/App.js
+++ budget_manager/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './index.css';
 import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
